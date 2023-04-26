8c911d054f4392c333550a4f1bdb6870c9268ebe
Toufikas <131577776+Toufikas@users.noreply.github.com>
2023-04-26 19:28:14 +0300

    Update client2.go

--- src/client2.go
+++ src/client2.go
@@ -34,19 +34,22 @@ func main() {
             defer conn.Close()
             // Create a buffer for incoming data.
             buf := make([]byte, 4096)
-for i := 0 ; i < 5 ; i++ {
-            // Read data from the connection.
-            n, err := conn.Read(buf)
-            if err != nil {
-                log.Fatal(err)
-            }
+str := []byte("wellcome")
+copy(buf, str)
+n := int(8)
+            for i := 0 ; i < 5 ; i++ {
+                // Echo the data back to the connection.
+                _, err := conn.Write(buf[:n])
+                if err != nil {
+                   log.Fatal(err)
+                }
 
-            // Echo the data back to the connection.
-            _, err = conn.Write(buf[:n])
-            if err != nil {
-                log.Fatal(err)
+                // Read data from the connection.
+                n, err = conn.Read(buf)
+                if err != nil {
+                    log.Fatal(err)
+                }
             }
-}
         }(conn)
     }
 }
